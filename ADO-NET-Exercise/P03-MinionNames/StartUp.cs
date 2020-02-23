a7651cdb10f3356c10be8da9d32dd561768b6382
Petko Pedev <petko.pedev@gmail.com>
2020-02-23 18:40:33 +0200

    Adding CSharp DB Repo with exercises from the SoftUni CSharp DB Course.

---
+++ ADO-NET-Exercise/P03-MinionNames/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace P03_MinionNames
+{
+    class StartUp
+    {
+        private static string connectionString = "Server=DESKTOP-E5PBLPH\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            using (connection)
+            {
+                string queryString = @"SELECT Name FROM Villains WHERE Id = @Id
+
+                                  SELECT ROW_NUMBER() OVER(ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ADO-NET-Exercise/P06-RemoveVillian/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace P06_RemoveVillian
+{
+    class Program
+    {
+        private static string connectionString = "Server=DESKTOP-E5PBLPH\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        private static SqlTransaction transaction;
+        static void Main(string[] args)
+        {
+            int id = int.Parse(Console.ReadLine());
+
+            connection.Open();
+
+            using (connection)
+            {
+                transaction = connection.BeginTransaction();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.Transaction = transaction;
+                command.CommandText = "SELECT Name FROM Villains WHERE Id = @villainId";
+                command.Parameters.AddWithValue("@villainId", id);
+
+                object value = command.ExecuteScalar();
+
+                if (value == null)
+                {
+                    Console.WriteLine("No such villain was found.");
+                }
+            }
+        }
+    }
+}
